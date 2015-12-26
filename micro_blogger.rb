4a82a98a75e880c3a0e898175d65dbffad4c0261
Obtuse <theobtuseautodidact@gmail.com>
2015-12-25 20:07:36 -0700

    I3 complete I can spam myself

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'pry'
 
 class MicroBlogger
   attr_reader :client
@@ -8,11 +9,32 @@ class MicroBlogger
     @client = JumpstartAuth.twitter
   end
 
+  def followers_list
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name.downcase }
+  end
+
+  def spam_my_followers(message)
+    followers_list.each  { |follower| dm(follower, message) }
+  end
+
+
   def tweet(message)
     if message.length <= 140
       @client.update(message)
     else
       puts "Warning: Message > 140 characters. This will not post to Twitter."
+    end
+  end
+
+  def dm(target, message)
+    puts "Trying to send #{target} this direct message:"
+    screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name.downcase } #added downcase to render case insensitive
+    if screen_names.include?(target)
+      puts message
+      message = "d @#{target} #{message}"
+      tweet(message)
+    else
+      puts "Sorry, you can only DM people who follow you."
     end
   end
 
@@ -27,6 +49,8 @@ class MicroBlogger
       case command
         when "q" then puts "Goodbye!"
         when "t" then tweet parts[1..-1].join(" ")
+        when "dm" then dm(parts[1], parts[2..-1].join(" "))
+        when "spam" then spam_my_followers parts[1..-1].join(" ")
         else
           puts "Sorry, I don't know how to #{command}"
       end
